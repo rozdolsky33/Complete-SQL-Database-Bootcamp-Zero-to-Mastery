2a589d0cde16eba2c1300a559ac68bfe36d3b591
Volodymyr Rozdolsky <32368688+rozdolsky33@users.noreply.github.com>
2021-01-16 14:53:03 -0500

    Update questions.sql

--- SQL Deep Dive/Comparison Operators/questions.sql
+++ SQL Deep Dive/Comparison Operators/questions.sql
@@ -2,6 +2,7 @@
 /*
 * Write your query here
 */
+Select count( * ) from customers where gender = 'F' AND state = 'OR';
 
 -- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
 /*
